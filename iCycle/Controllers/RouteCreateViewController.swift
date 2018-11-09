8da1d738c34a8a18c4d4e22bb3ba4a194aed283f
Austin McPhail <austin.mcphail@hotmail.com>
2018-11-09 02:57:43 -0600

    Removed submodule

---
+++ iCycle/Controllers/RouteCreateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoutesCreateViewController.swift
+//  iCycle
+//
+//  Created by Austin McPhail on 2018-11-09.
+//  Copyright © 2018 Valentyna Akulova. All rights reserved.
+//
+
+import UIKit
+
+class RoutesCreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iCycle/Theme/ChameleonShorthand.swift
@@ -0,0 +1,9 @@
+//
+//  ChameleonShorthand.swift
+//  iCycle
+//
+//  Created by Austin McPhail on 2018-11-09.
+//  Copyright © 2018 Valentyna Akulova. All rights reserved.
+//
+
+import Foundation
