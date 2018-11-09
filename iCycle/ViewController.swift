0edd886b154bbd5b98ef354899b67a32749e3e1d
Valentyna Akulova <akulovav@udml18.local>
2018-11-08 18:43:26 -0600

    Initial Commit

---
+++ iCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCycle
+//
+//  Created by Valentyna Akulova on 2018-11-08.
+//  Copyright © 2018 Valentyna Akulova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
