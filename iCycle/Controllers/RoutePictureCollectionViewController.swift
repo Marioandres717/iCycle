50f9c0f5e5c1b8e355ee7909c18809e099bfb633
mario andres <marioarendon2@gmail.com>
2018-12-03 21:13:30 -0600

    route pictures stuff

---
+++ iCycle/Controllers/RoutePictureCollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoutePictureCollectionViewController.swift
+//  iCycle
+//
+//  Created by Mario Rendon Zapata on 2018-12-03.
+//  Copyright © 2018 Valentyna Akulova. All rights reserved.
+//
+
+import UIKit
+
+class RoutePictureCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
