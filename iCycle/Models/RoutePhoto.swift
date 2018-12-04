86cf2dd87c94144742e21f714ebcc16ef88afbbc
Austin McPhail <austin.mcphail@hotmail.com>
2018-12-03 21:16:10 -0600

    upgraded RoutePhoto

--- iCycle/Models/RoutePhoto.swift
+++ iCycle/Models/RoutePhoto.swift
@@ -8,21 +8,41 @@
 
 import Foundation
 import GoogleMaps
+import Firebase
 
 class RoutePhoto {
-    var photo: String
+    var photoUrl: String
+    var photoImage: UIImage
     var long: Double
     var lat: Double
     var user: User
     var caption: String?
     var title: String
     
-    init(photo: String, long: Double, lat: Double, user: User, title: String, caption: String?) {
-        self.photo = photo
+    init(photoUrl: String, long: Double, lat: Double, user: User, title: String, caption: String?) {
+        self.photoUrl = photoUrl
         self.long = long
         self.lat = lat
         self.user = user
         self.title = title
         self.caption = caption
+        self.photoImage = UIImage(named: "placeholder")!
+        
+        let urlToImage = self.photoUrl
+        if urlToImage != "" {
+            let storageRef = Storage.storage().reference(forURL: urlToImage)
+            let maxSize: Int64 = 3 * 1024 * 1024 // 3MB
+            storageRef.getData(maxSize: maxSize) { (data, error) in
+                if let error = error {
+                    print(error)
+                    self.photoImage = UIImage(named: "placeholder")!
+                } else {
+                    print("Sucessfully fetched image")
+                    self.photoImage = UIImage(data: data!)!
+                }
+            }
+        } else {
+            self.photoImage = UIImage(named: "placeholder")!
+        }
     }
 }
